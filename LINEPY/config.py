74ac7239bde9b606b625e9119ceac56c8fd90753
Galank-07 <31801682+Galank-07@users.noreply.github.com>
2018-07-11 17:37:14 +0800

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -28,7 +28,7 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.0.3'
+    APP_VER     = '1.4.17'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'CHROMEOS'
     SYSTEM_VER  = '10.12.0'
